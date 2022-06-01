eb42499e3a380f640d5a80d0b4d57fc171fb513e
eduardomarreiro <eduardo4pc@outlook.com>
2022-06-01 15:33:59 -0300

    Added income route

--- routes/order.js
+++ routes/order.js
@@ -1,3 +1,4 @@
+const res = require("express/lib/response");
 const Order = require("../models/Order");
 const { 
         verifyToken,
@@ -65,5 +66,34 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
         res.status(500).json(err); 
     }
 });
+
+// GET MONTHLY INCOME
+
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+  
+    try {
+      const income = await Order.aggregate([
+        { $match: { createdAt: { $gte: previousMonth } } },
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+            sales: "$amount",
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: "$sales" },
+          },
+        },
+      ]);
+      res.status(200).json(income);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
